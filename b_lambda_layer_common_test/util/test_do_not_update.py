3d894c81daf4c800580ee0ebcbf2b541e22eed26
Robertas Stankevičius <robertas.stankevicius@biomapas.com>
2020-12-03 10:30:57 +0200

    Add test.

---
+++ b_lambda_layer_common_test/util/test_do_not_update.py
@@ -0,0 +1,10 @@
+from b_lambda_layer_common.source.python.b_lambda_layer_common.util.do_not_update import DoNotUpdate
+
+
+def test_FUNC_init_WITH_no_params_EXPECT_class_initiated() -> None:
+    """
+    Checks whether class initialization works.
+
+    :return: No return.
+    """
+    DoNotUpdate()
