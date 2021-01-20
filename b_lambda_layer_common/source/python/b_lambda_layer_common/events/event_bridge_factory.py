13277b0b03b008c9d4facb345696b19be4528559
laimonassutkus <laimonas.sutkus@gmail.com>
2021-01-20 16:31:43 +0200

    Add ability to conveniently emit EventBridge events.

---
+++ b_lambda_layer_common/source/python/b_lambda_layer_common/events/event_bridge_factory.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from typing import Dict, Any, Optional
+
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+try:
+    from b_lambda_layer_common.exceptions.container.internal_error import InternalError
+except ImportError as ex:
+    logger.exception(f'Failed import.')
+    from b_lambda_layer_common.source.python.b_lambda_layer_common.exceptions.container.internal_error import InternalError
+
+
+class EventBridgeFactory:
+    __client = boto3.client('events')
+
+    def __init__(
+            self,
+            source: str,
+            detail_type: str,
+            detail: Dict[Any, Any],
+            event_bus_name: Optional[str] = None
+    ) -> None:
+        self.__source = source
+        self.__detail_type = detail_type
+        self.__detail = detail
+        self.__event_bus_name = event_bus_name
+
+    def emit(self, boto_client: Optional[Any] = None) -> None:
+        event_entry = dict(
+            Source=self.__source,
+            DetailType=self.__detail_type,
+            EventBusName=self.__event_bus_name,
+            Detail=json.dumps(self.__detail),
+        )
+
+        try:
+            response = (boto_client or self.__client).put_events(Entries=[event_entry])
+
+            if response['FailedEntryCount'] != 0:
+                errors = '\n'.join(entry.get('ErrorMessage') for entry in response['Entries'])
+                raise InternalError(f'Failed to emit events: {errors}.')
+        except ClientError as ex:
+            raise InternalError(f'Failed to emit events ({str(ex)}).')
---
+++ b_lambda_layer_common_test/unit/events/test_event_bridge_factory.py
@@ -0,0 +1,20 @@
+from b_lambda_layer_common.source.python.b_lambda_layer_common.events.event_bridge_factory import EventBridgeFactory
+
+
+def test_FUNC_emit_WITH_valid_event_EXPECT_event_successful() -> None:
+    """
+    Test the default value for message.
+
+    :return: No return.
+    """
+    class DummyBoto:
+        def put_events(self, *args, **kwargs):
+            return {'FailedEntryCount': 0}
+
+    factory = EventBridgeFactory(
+        source='Test',
+        detail_type='Test',
+        detail={}
+    )
+
+    factory.emit(DummyBoto())
