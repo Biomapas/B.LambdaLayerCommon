2cbe697da22347080c8bba2ac444ab1626aecf28
Matas Gumbinas <matas.gumbinas@biomapas.com>
2021-04-19 14:20:28 +0300

    Add botocore package version setting

--- b_lambda_layer_common/boto3_version.py
+++ b_lambda_layer_common/boto3_version.py
@@ -1,33 +1,11 @@
-from enum import Enum
-from typing import Optional
+from b_lambda_layer_common.package_version import PackageVersion
 
 
-class Boto3Version:
-    class Boto3VersionType(Enum):
-        NONE = 'NONE'
-        SPECIFIC = 'SPECIFIC'
-        LATEST = 'LATEST'
+class Boto3Version(PackageVersion):
+    """
+    Deprecated ``boto3`` package version identifier.
 
-    def __init__(self, version: Optional[str] = None, version_type: Optional[Boto3VersionType] = None):
-        self.__version = version
-        self.__version_type = version_type
+    Use general ``PackageVersion`` class instead.
+    """
 
-    @property
-    def version_string(self) -> Optional[str]:
-        return self.__version
-
-    @property
-    def version_type(self) -> Boto3VersionType:
-        return self.__version_type
-
-    @classmethod
-    def from_string_version(cls, version_string: str) -> 'Boto3Version':
-        return cls(version=version_string, version_type=cls.Boto3VersionType.SPECIFIC)
-
-    @classmethod
-    def latest(cls) -> 'Boto3Version':
-        return cls(version_type=cls.Boto3VersionType.LATEST)
-
-    @classmethod
-    def dont_install(cls) -> 'Boto3Version':
-        return cls(version_type=cls.Boto3VersionType.NONE)
+    pass
