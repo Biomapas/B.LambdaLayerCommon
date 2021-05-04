1725d0eccf488c310e3c9a107a33922a7e97eae9
Robertas Stankevičius <robertas.stankevicius@biomapas.com>
2021-05-04 15:56:47 +0300

    Add PynamoDB encoder for MapAttribute type serialization support.

---
+++ b_lambda_layer_common_test/unit/util/test_pynamodb_encoder.py
@@ -0,0 +1,27 @@
+import json
+from decimal import Decimal
+
+from pynamodb.attributes import MapAttribute
+
+from b_lambda_layer_common.source.python.b_lambda_layer_common.util.pynamodb_encoder import PynamoDBEncoder
+
+
+def test_FUNC_default_WITH_map_attribute_EXPECT_successful_serialization() -> None:
+    """
+    Checks whether serialization works.
+
+    :return: No return.
+    """
+    d = MapAttribute(attributes={
+        'key': Decimal(1),
+        'key2': {1, 2, 3, 3, 3, 3}
+    })
+
+    try:
+        json.dumps(d)
+    except TypeError:
+        pass
+    else:
+        raise AssertionError('Expected to fail.')
+
+    json.dumps(d, cls=PynamoDBEncoder)
