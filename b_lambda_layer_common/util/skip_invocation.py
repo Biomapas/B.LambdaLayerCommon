067a3e253646f5c1f0270f81a71201e08cda3825
Edvinas Bosas <edvinas.bosas@biomapas.com>
2021-12-08 14:31:12 +0200

    Make skip_invocation decorator's callable argument actually useful.

--- b_lambda_layer_common/util/skip_invocation.py
+++ b_lambda_layer_common/util/skip_invocation.py
@@ -2,13 +2,15 @@ import functools
 from typing import Any, Callable, Dict, Optional, Union
 
 
-def skip_invocation(determinator: Optional[Union[str, bool, Callable]] = None) -> Union[Callable, None]:
+def skip_invocation(determinator: Optional[Union[str, bool, Callable[[Dict, Any], bool]]] = None) -> Union[Callable, None]:
     """
     A decorator which allows to skip decorated function's execution if certain conditions are met.
     By default it returns decorated function's execution if no arguments are provided.
     If a determinator of type 'str' is provided, the decorator checks whether this key exists function's event 'dict' as parses result as 'bool' type.
     If a determinator of type 'bool' is provided, the decorator parses its value.
     If a callable object is provided as determinator, the decorator parses its value as 'bool'.
+    Callable should accept 2 arguments - Lambda function's event and context.
+    
     If any of the above evaluate to 'bool' of value True, the decorated function is not executed.
 
     :param determinator: An object which is used to evaluate whether to execute the decorated function.
@@ -47,10 +49,11 @@ def skip_invocation(determinator: Optional[Union[str, bool, Callable]] = None) -
     >>>
 
     When a callable is provided which evaluates to True.
+    Callable should take 2 arguments - Lambda function's event and context.
 
     >>> item = {'heartbeat': True}
-    >>> is_even = lambda x: x % 2 == 0
-    >>> @skip_invocation(determinator=is_even(4))
+    >>> is_heartbeat = lambda e, c: "heartbeat" in e
+    >>> @skip_invocation(determinator=is_heartbeat)
     ... def handler(event):
     ...     return event
     >>> handler(item)
@@ -61,13 +64,14 @@ def skip_invocation(determinator: Optional[Union[str, bool, Callable]] = None) -
         @functools.wraps(func)
         def wrapped_f(*args, **kwargs):
             skip = False
+            event: Dict[str, Any] = args[0]
+            context: Any = args[1]
             if isinstance(determinator, str):
-                event: Dict[str, Any] = args[0]
                 skip = bool(event.get(determinator, False))
             elif isinstance(determinator, bool):
                 skip = determinator
             elif callable(determinator):
-                skip = bool(determinator())
+                skip = bool(determinator(event, context))
             
             if skip:
                 return
